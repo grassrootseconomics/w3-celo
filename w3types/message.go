759de1497e68f1435b05a59379301f0dfac9f704
Mohammed Sohail <sohailsameja@gmail.com>
2024-05-22 13:42:56 +0800

    w3types: Fixed incorrect Message struct names when marshaling to JSON

--- w3types/message.go
+++ w3types/message.go
@@ -98,8 +98,8 @@ type message struct {
 	To         *common.Address  `json:"to,omitempty"`
 	Nonce      hexutil.Uint64   `json:"nonce,omitempty"`
 	GasPrice   *hexutil.Big     `json:"gasPrice,omitempty"`
-	GasFeeCap  *hexutil.Big     `json:"gasFeeCap,omitempty"`
-	GasTipCap  *hexutil.Big     `json:"gasTipCap,omitempty"`
+	GasFeeCap  *hexutil.Big     `json:"maxFeePerGas,omitempty"`
+	GasTipCap  *hexutil.Big     `json:"maxPriorityFeePerGas,omitempty"`
 	Gas        hexutil.Uint64   `json:"gas,omitempty"`
 	Value      *hexutil.Big     `json:"value,omitempty"`
 	Input      hexutil.Bytes    `json:"data,omitempty"`
